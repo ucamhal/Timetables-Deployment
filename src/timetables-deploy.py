c98da6bbfc5812b6ca6b559a7efdf41503b26167
Hal Blackburn <hal.blackburn@gmail.com>
2012-01-05 10:27:42 +0000

    Added ISC license.

--- src/timetables-deploy.py
+++ src/timetables-deploy.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+# Copyright (c) 2011, CARET, University of Cambridge <hwtb2@caret.cam.ac.uk>
+# 
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+# Non standard dependancies:
+# - argparse: either use Python >= 2.7 or easy_install argparse
+# - git: GitPython: http://gitorious.org/git-python   easy_install gitpython
+
 """
 A tool to automate deployment of the the Mercury project Timetables web app. 
 The code is obtained from a specific tag in a git repository, then external
@@ -9,9 +27,6 @@ The aim is to minimise the potential for human errror when assembling a
 Timetables deployment, allowing greater confidence that a given deployment can 
 be repeated.
 """
-# Non standard dependancies:
-# - argparse: either use Python >= 2.7 or easy_install argparse
-# - git: GitPython: http://gitorious.org/git-python   easy_install gitpython
 
 import argparse, collections, git, tempfile, os, shutil, sys, pwd, grp
 from os.path import join
